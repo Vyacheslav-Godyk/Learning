bdaec35a538836093da16de94f8b7c143135d6bd
Vyacheslav-Godyk <slavahodyk@gmail.com>
2025-02-15 09:24:45 +0200

    Lesson 2.5

---
+++ JS/HW_JS/Lesson 2/Lesson 2.4/scripts/main.js
@@ -0,0 +1,23 @@
+let users = [
+    {name: 'user1', username: 'Jhony', password: 'asdasd1'},
+    {name: 'user2', username: 'Kevin', password: '1243qedasd'},
+    {name: 'user3', username: 'Maykl', password: 'dsad12'},
+    {name: 'user4', username: 'Anton', password: 'dasdasd1'},
+    {name: 'user5', username: 'Anna', password: 'asdasd1'},
+    {name: 'user6', username: 'Monika', password: 'vfcvszcxvx!@E#'},
+    {name: 'user7', username: 'Julia', password: 'dasd1!@#'},
+    {name: 'user8', username: 'Antonina', password: 'ASDSAD!231'},
+    {name: 'user9', username: 'Aleksandra', password: 'dasdasd23123'},
+    {name: 'user10', username: 'Violetta', password: '$!@dasd!@$1'},
+]
+
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password)
---
+++ JS/HW_JS/Lesson 2/Lesson 2.5/scripts/main.js
@@ -0,0 +1,11 @@
+let temps = [
+    {day: 'Monday', morningTemp: 15, dayTemp: 20, nightTemp: 11},
+    {day: 'Tuesday', morningTemp: 5, dayTemp: 10, nightTemp: 7},
+    {day: 'Wednesday', morningTemp: 9, dayTemp: 11, nightTemp: 5},
+    {day: 'Thursday', morningTemp: 3, dayTemp: 13, nightTemp: 0},
+    {day: 'Friday', morningTemp: 9, dayTemp: 27, nightTemp: 5},
+    {day: 'Saturday', morningTemp: 5, dayTemp: 11, nightTemp: 1},
+    {day: 'Sunday', morningTemp: 13, dayTemp: 18, nightTemp: -1},
+]
+
+console.log(temps[0].day)
