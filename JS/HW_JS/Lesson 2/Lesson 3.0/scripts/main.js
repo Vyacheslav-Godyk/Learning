004e1222c3caac8d651eec37eda60bbe1ef95f59
Vyacheslav-Godyk <slavahodyk@gmail.com>
2025-02-18 17:00:38 +0200

    HW JS Lesson 3.0

--- JS/HW_JS/Lesson 2/Lesson 3.0/scripts/main.js
+++ JS/HW_JS/Lesson 2/Lesson 3.0/scripts/main.js
@@ -1,2 +1,12 @@
 let number1 = +prompt('Enter number 1');
 let number2 = +prompt('Enter number 2');
+
+if (number1 > number2) {
+    console.log(number1);
+} else if (number1 < number2) {
+    console.log(number2);
+} else if (number1 === number2) {
+    console.log('Введені рівні числа');
+} else {
+    console.log('Wrong');
+}
