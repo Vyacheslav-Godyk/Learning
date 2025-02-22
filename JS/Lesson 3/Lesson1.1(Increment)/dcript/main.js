30983de3592718976403691fabb5d8148337f479
Vyacheslav-Godyk <slavahodyk@gmail.com>
2025-02-22 20:35:23 +0200

    Lesson 3

--- JS/Lesson 3/Lesson1.1(Increment)/dcript/main.js
+++ JS/Lesson 3/Lesson1.1(Increment)/dcript/main.js
@@ -1,2 +1,39 @@
-let x = 0;
-x = x + 1
+let a = 0;
+let b = a++;
+console.log(a, b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// x = x + 1;
+//
+// console.log(x)
+// x++;
+// console.log(x);
+// x += 1;
+// console.log(x)
+//
+// x = x - 1;
+// console.log(x)
+// x -= 1;
+// console.log(x);
+// x--;
+// console.log(x)
+
+
